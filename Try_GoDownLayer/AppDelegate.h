aea7bf5f5cca80492041de2360f035b52fc2e92a
irons163 <irons@ironsdeMacBook-Air.local>
2015-05-06 07:56:19 +0800

    Initial Commit

---
+++ Try_GoDownLayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Try_GoDownLayer
+//
+//  Created by irons on 2015/5/6.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
